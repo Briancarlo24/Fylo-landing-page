9914ea3169a1ab6dc236318018d70bec98598633
Briancarlo24 <Briancarlo2494@gmail.com>
2021-11-17 13:53:14 +0800

    finished

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const btn1 = document.querySelector(".btn-1");
 const btn2 = document.querySelector(".btn-2");
 
-const email2 = document.getElementById("email-2").value;
-
-function validate() {
-  let email = document.getElementById("email-1").value;
+function validate(email, num) {
+  const input = document.getElementById(`email-${num}`);
+  const pop = document.querySelector(`.pop-up-${num}`);
   if (validateEmail(email)) {
+    pop.classList.remove("display");
+    input.classList.remove("error-border");
     alert("email " + email + " is valid");
     return true;
+  } else {
+    pop.classList.add("display");
+    input.classList.add("error-border");
   }
 
   // Add Error Message to check if email is valid
@@ -19,4 +23,11 @@ function validateEmail(email) {
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
-btn1.addEventListener("click", validate);
+btn1.addEventListener("click", function () {
+  const email = document.getElementById("email-1").value;
+  validate(email, 1);
+});
+btn2.addEventListener("click", function () {
+  const email = document.getElementById("email-2").value;
+  validate(email, 2);
+});
